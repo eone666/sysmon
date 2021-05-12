c13fdba097e6c6096c7e88b830f5def78596f6cf
eone666 <eone.ukg@gmail.com>
2021-05-12 23:27:18 +0600

    running new process

--- Windows/RunWindow.axaml.cs
+++ Windows/RunWindow.axaml.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Diagnostics;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 
 namespace sysmon.Windows
@@ -17,6 +20,27 @@ namespace sysmon.Windows
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private async void OpenFileDialog(object? sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+
+            string[] result = await dialog.ShowAsync(this);
+
+            if (result != null && result.Length > 0)
+            {
+                try
+                {
+ 
+                        Process.Start(result[0]);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine(exception);
+                    throw;
+                }
+            }
         }
     }
 }
